4832b254b670cb9a30ef59a8924e1db8c61bc276
linchqd <13435600095@163.com>
2019-04-15 13:33:06 +0800

    time

---
+++ python_time.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+# _*_ coding _*_
+# time模块
+
+# 表示时间的方式
+#   1.时间戳(timestamp)：通常来说，时间戳表示的是从1970年1月1日00:00:00开始按秒计算的偏移量。
+#   2.格式化的时间字符串(Format String)
+#   3.结构化的时间(struct_time)：struct_time元组共有9个元素共九个元素:(年，月，日，时，分，秒，一年中第几周，一年中第几天，夏令时)
+
+# 转换方法:
+#   1.时间戳 --> 结构化的时间 time.localtime(time.time())  time.gmtime(time.time())
+#   2.结构化的时间 --> 时间戳 time.mktime(time.localtime())
+#   3.结构化的时间 --> 格式化的时间字符串  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) 
+#   4.格式化的时间字符串 --> 结构化的时间  time.strptime('2019-04-03 15:39:53','%Y-%m-%d %H:%M:%S')
+#   5.时间戳 --> 'Wed Apr  3 15:59:42 2019'  time.ctime(time.time())
+#   6.结构化的时间 --> 'Wed Apr  3 15:59:42 2019'  time.asctime(time.localtime())
+import time
